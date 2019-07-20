53fd41cc1ebbe7614bce96cf163e9d67c8fc1ecd
Ahmed <ahmedhussein.ah96@gmail.com>
2019-07-20 02:25:04 +0200

    Initialization

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, Response
+from LipNet.evaluation.predict import predict
+
+app = Flask(__name__)
+
+
+@app.route('/lips-to-text', methods=['POST'])
+def lips_to_text():
+    video = request.files.get("video")
+    if video == None:
+        return Response("No video received", status=400)
+
+    vname = "video." + video.filename.split(".")[-1]
+    video.save(vname)
+    video, result = predict("LipNet/evaluation/models/overlapped-weights368.h5", vname)
+    return Response(result)
+
+
+@app.route("/")
+def index():
+    return "<h1>LipsReaderAPI...Running<h1>";
+
+
+if __name__ == "__main__":
+    app.run(threaded=False)
